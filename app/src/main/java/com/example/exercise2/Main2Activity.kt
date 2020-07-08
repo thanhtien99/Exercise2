0c6cccf9a2877f141fbc2fda58616cd5571f45e5
Lê Thành Tiến <lethanhtien532@gmail.com>
2020-07-08 13:19:10 +0700

    My First Commit

---
+++ app/src/main/java/com/example/exercise2/Main2Activity.kt
@@ -0,0 +1,14 @@
+package com.example.exercise2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Main2Activity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.relative_layout)
+
+    }
+}
